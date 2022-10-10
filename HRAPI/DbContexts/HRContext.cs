79668361587f10bc70b909a25a97912c7978e6a9
lutfiqaraman <lutfiqaraman@gmail.com>
2022-10-10 20:54:27 +0300

    build the dbcontext

---
+++ HRAPI/DbContexts/HRContext.cs
@@ -0,0 +1,17 @@
+﻿using HRAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRAPI.DbContexts
+{
+    public class HRContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; } = null!;
+        public DbSet<Employee> Employees { get; set; } = null!;
+
+        public HRContext(DbContextOptions options) 
+            : base(options)
+        {
+
+        }
+    }
+}
