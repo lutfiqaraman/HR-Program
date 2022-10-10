600067a186533a49968a9751db66ce5a8fb3845e
lutfiqaraman <lutfiqaraman@gmail.com>
2022-10-10 20:47:27 +0300

    build the entity

--- HRAPI/Models/Department.cs
+++ HRAPI/Models/Department.cs
@@ -1,9 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HRAPI.Models
 {
     [Table("Department")]
     public class Department
     {
+        [Key]
+        public int Id { get; set; }
+        public string? DepartmentName { get; set; }
+        public ICollection<Employee>? Employees { get; set; }
     }
 }
--- HRAPI/Models/Employee.cs
+++ HRAPI/Models/Employee.cs
@@ -1,9 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HRAPI.Models
 {
     [Table("Employee")]
     public class Employee
     {
+        [Key]
+        public int Id { get; set; }
+        public string? EmployeeName { get; set; }
+        public int DepartmentId { get; set; }
+        public Department? Department { get; set; }
     }
 }
