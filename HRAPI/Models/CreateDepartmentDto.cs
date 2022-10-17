2ea3badb744a4bba8d15ae84ac87e6033f1d4146
lutfiqaraman <lutfiqaraman@gmail.com>
2022-10-17 19:39:31 +0300

    Create a department

---
+++ HRAPI/Models/CreateDepartmentDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRAPI.Models
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage = "name cannot be empty, you should a provide a value")]
+        [MaxLength(200)]
+        public string? DepartmentName { get; set; }
+    }
+}
