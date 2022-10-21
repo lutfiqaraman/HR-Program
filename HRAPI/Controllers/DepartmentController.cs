cebe3fa9f677342aa4748e31cba634cf76d4806e
lutfiqaraman <lutfiqaraman@gmail.com>
2022-10-21 19:18:27 +0300

    insert department

---
+++ HRAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,33 @@
+﻿using HRAPI.Entities;
+using HRAPI.Repository.DepartmentRepo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRAPI.Controllers
+{
+    [Route("api/department")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IDepartmentRepository departmentRepository;
+
+        public DepartmentController(IDepartmentRepository DepartmentRepository)
+        {
+            departmentRepository = 
+                DepartmentRepository ?? throw new ArgumentNullException(nameof(DepartmentRepository));
+        }
+
+        [HttpPost]
+        [Route("AddDepartment")]
+        public async Task<IActionResult> InsertDepartment(Department department)
+        {
+            Department? result =   
+                await departmentRepository.InsertDepartment(department);
+
+            if (department is null)
+                return StatusCode(StatusCodes.Status500InternalServerError, "something is wrong");
+
+            return Ok("Department has been added successfully");
+        }
+    }
+}
