e08c58c0ee217e0c23081ba0596d8047afaf19dc
lutfiqaraman <lutfiqaraman@gmail.com>
2022-11-01 20:17:04 +0200

    build employee

---
+++ HRAPI/Models/EmployeeDtos/EmployeeDto.cs
@@ -0,0 +1,12 @@
+﻿using HRAPI.Entities;
+
+namespace HRAPI.Models.EmployeeDtos
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        public string? EmployeeName { get; set; }
+        public int DepartmentId { get; set; }
+        public Department? Department { get; set; }
+    }
+}
