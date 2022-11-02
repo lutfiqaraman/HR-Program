b32cdbcca3791073c103bc5e2e9d6e081aabe945
lutfiqaraman <lutfiqaraman@gmail.com>
2022-11-02 19:24:41 +0200

    build employee repository

---
+++ HRAPI/Repository/EmployeeRepo/EmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HRAPI.DbContexts;
+
+namespace HRAPI.Repository.EmployeeRepo
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly HRContext context;
+        private readonly IMapper mapper;
+
+        public EmployeeRepository(HRContext Context, IMapper Mapper)
+        {
+            context =
+                Context ?? throw new ArgumentNullException(nameof(Context));
+
+            mapper =
+                Mapper ?? throw new ArgumentNullException(nameof(Mapper));
+        }
+
+        public Task GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetEmployeeByID(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
